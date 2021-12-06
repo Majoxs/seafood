765c4745548db57bb1ae00170922ab734d247996
Majoxs <majoxs@gmail.com>
2021-12-06 15:02:03 +0500

    Added adaptive

--- js/main.js
+++ js/main.js
@@ -5,6 +5,17 @@ $(function () {
     prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
     vertical: true,
+    responsive: [
+      {
+        breakpoint: 371,
+        settings: {
+          dots: false,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 2000,
+        }
+      }
+    ]
   });
 
   $('.product__name').slick({
@@ -15,6 +26,26 @@ $(function () {
     vertical: true,
     nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
     prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 891,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          arrows: false,
+          dots: true,
+        }
+      },
+      {
+        breakpoint: 461,
+        settings: {
+          vertical: false,
+          slidesToShow: 1,
+          arrows: false,
+          dots: true,
+        }
+      }
+    ]
   });
 
   $('.product__content').slick({
@@ -24,5 +55,9 @@ $(function () {
     fade: true,
     arrows: false,
   });
+
+  document.querySelector('.menu__btn').addEventListener('click', () => {
+    document.querySelector('.menu__list').classList.toggle('menu__list--active');
+  })
 
 });
